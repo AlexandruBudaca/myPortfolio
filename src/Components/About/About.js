5ec11625addb09588837d2f98131107bf18b7ae4
Alex <alex.andrul10@gmail.com>
2020-05-30 14:53:12 +0100

    change img

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,20 +1,18 @@
 import React from "react";
-import Logos from "./Logos";
+import aboutImg from "../../JeOld.png";
 
 const About = () => {
   return (
     <section>
       <div className="about">
-        <img
-          className="img-about"
-          src="https://steamuserimages-a.akamaihd.net/ugc/852722377424981834/4DA318C949A0452E56C5F68B10636DA4F2071C56/"
-          alt="profile"
-        />
+        <div className="img-div-about">
+          <img className="img-about" src={aboutImg} alt="profile" />
+        </div>
+
         <div className="about-content">
           <h1>Who am I?</h1>
           <h2>An actor who plays with technology!</h2>
           <p>
-            {" "}
             If you know already that I am an actor, that means that you know the
             story, but what you don't know it's how it ends. I've studied acting
             since I was in the middle school, but also I've always had a sort of
@@ -29,7 +27,6 @@ const About = () => {
           </p>
         </div>
       </div>
-      <Logos />
     </section>
   );
 };
