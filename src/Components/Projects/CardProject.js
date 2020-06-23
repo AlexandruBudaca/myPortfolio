fe3c6fa1e8987383fe4fa0ef79fa200b9eca7cfc
Alex <35708592+AlexandruBudaca@users.noreply.github.com>
2020-06-23 08:16:37 +0100

    projects

---
+++ src/Components/Projects/CardProject.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const CardProject = props => {
+  return (
+    <div className="card-project">
+      {" "}
+      <img src={props.imgSrc} alt="project-img" />
+      <div>
+        <h4>Countries</h4>{" "}
+        <p>
+          In publishing and graphic design, Lorem ipsum is a placeholder text
+          commonly used to demonstrate.
+        </p>
+      </div>
+      <div className="btn-card-project">
+        <button>
+          {" "}
+          <i className="fab fa-github" />
+        </button>
+        <button>See Live</button>
+      </div>
+    </div>
+  );
+};
+export default CardProject;
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,10 +1,30 @@
 import React from "react";
+import CardProject from "./CardProject";
 
 const Projects = () => {
   return (
-    <div className="projects">
-      <h2 className="about-me">Projects</h2>
+    <div className="projects-main">
+      <h2>Projects ></h2>
       <hr />
+      <div className="projects">
+        <div className="container-projects">
+          <CardProject
+            imgSrc={
+              "https://coryrylan.com/assets/images/posts/types/javascript-1280x960.png"
+            }
+          />
+          <CardProject
+            imgSrc={
+              "https://www.freecodecamp.org/news/content/images/2020/01/js-image.jpeg"
+            }
+          />
+          <CardProject
+            imgSrc={
+              "https://images.ctfassets.net/yr4qj72ki4ky/legacyBlogPost77Thumbnail/cd4783ad7b35efc4367166a570a9952e/bigstock-Real-Java-Script-Code-Developi-217215433.jpg"
+            }
+          />
+        </div>
+      </div>
     </div>
   );
 };
