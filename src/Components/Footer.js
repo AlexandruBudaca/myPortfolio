81f5ebaa2233df76ea091b06a2bd5274b4d3d9ec
Alex <35708592+AlexandruBudaca@users.noreply.github.com>
2020-06-23 11:28:24 +0100

    no footer logo

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,8 @@
 import React from "react";
-import logo from "../../public/apple-touch-icon 2.png";
+
 const Footer = () => {
   return (
     <div className="footer">
-      <img src={logo} alt="footer-logo" />
       <h5> © Alexandru Budaca</h5>
     </div>
   );
